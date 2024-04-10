86adc6249ec96bc73480b5e502f2dc56438e01d6
Jenil Padshala <jenilpadshala@gmail.com>
2024-04-10 22:33:04 +0530

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -4,31 +4,6 @@ sudo apt-get update
 
 sudo apt install -y python3-pip nginx
 
-#sudo vim /etc/nginx/sites-enabled/fastapi_nginx
-
-
-# Define the filename
-filename="/etc/nginx/sites-enabled/fastapi_nginx"
-
-
-
-# Get the IP address from user input
-read -p "Enter the server IP address: " server_ip
-
-# Add the configuration to the file
-echo "server {" >> "$filename"
-echo "    listen 80;" >> "$filename"
-echo "    server_name $server_ip;" >> "$filename"
-echo "    location / {" >> "$filename"
-echo "        proxy_pass http://127.0.0.1:8000;" >> "$filename"
-echo "    }" >> "$filename"
-echo "}" >> "$filename"
-
-echo "Configuration added to $filename."
-
-
-sudo service nginx restart
-
 pip install --upgrade pip
 
 git clone https://github.com/JenilPadshala/job-rec-sys.git
